3797a84ba672888ec0ce232a3457798ae9dda070
Mohammad Syam <mosyam0202@gmail.com>
2024-11-04 20:59:30 +0700

    animasi

--- script.js
+++ script.js
@@ -12,3 +12,11 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     );
 });
+
+const cursor = document.querySelector('.cursor');
+document.addEventListener('mousemove', (e) => {
+    cursor.style.left = e.pageX + 'px';
+    cursor.style.top = e.pageY + 'px';
+})
+
+
